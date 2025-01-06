6440da96bd04ffed19ea679e8879c1b35d684692
Yandrew9393 <ywiullem@gmail.com>
2025-01-06 16:41:35 -0300

    algumas correcoes

--- src/script/index.js
+++ src/script/index.js
@@ -1,35 +1,15 @@
-const menu = document.querySelector(".menu");
-const nav = document.querySelector(".nav")
-const botao_formulario = document.querySelector("#botao-form")
+const menuButton = document.querySelector('.menu');
+const navMenu = document.querySelector('.nav');
 
+// Exibir/ocultar menu ao clicar no ícone de hambúrguer
+menuButton.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
 
-//Menu hamburger
-menu.addEventListener("click", ()=>{
-
-    nav.classList.toggle("desbloquear-menu");
-
-    console.log(nav)
-
-})
-
-
-botao_formulario.addEventListener("click", ()=>{
-
-const nome = document.querySelector("#nome.value");
-const email = document.querySelector("#email.value");
-const tel  = document.querySelector("#tel.value");
-const msg = document.querySelector("#text.value")
-
-
-
-if(nome == null || email == null || tel== null || msg == null ){
-
-    window.alert("Preencha todo o formulário");
-    
-}
-
-
-})
-
-
-
+// Fechar o menu ao clicar em um link
+const navLinks = document.querySelectorAll('.nav ul li a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+    });
+});
